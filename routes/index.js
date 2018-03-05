b06c10a6911d9e9386f9fbf655e30a5e32541db1
kevinmartinez <kevin@pixelkojan.se>
2018-03-05 17:04:45 +0100

    WIP ejs partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
 // Set up routes
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
-    res.render('index.ejs')
-  })
+  app.get('/', (req, res) => res.render('pages/index'))
+
+  app.get('/cars', (req, res) => res.render('pages/cars'))
 }
