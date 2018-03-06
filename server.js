7b3cfff543d0deb778100aab3aad4fb43d7ff881
kevinmartinez <kevin@pixelkojan.se>
2018-03-06 13:22:49 +0100

    WIP markup for car brands

--- server.js
+++ server.js
@@ -3,23 +3,23 @@
 const express = require('express')
 const port = process.env.PORT || 3000
 const routes = require('./routes/index.js')
-const sass = require('node-sass-middleware')
+// const sass = require('node-sass-middleware')
 const path = require('path')
 const app = express()
 
-app.use(sass({
-  /* Options */
-  src: __dirname,
-  dest: path.join(__dirname, 'public/css'),
-  debug: true,
-  outputStyle: 'compressed',
-  prefix: '/prefix' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-}))
+// app.use(sass({
+//   /* Options */
+//   src: __dirname,
+//   dest: path.join(__dirname, 'public/css'),
+//   debug: true,
+//   outputStyle: 'compressed',
+//   prefix: '/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+// }))
 // Note: you must place sass-middleware *before* `express.static` or else it will
 // not work.
-// app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/public', express.static(path.join(__dirname, '/public')))
 
-app.use('/public', express.static(process.cwd() + '/public'))
+// app.use('/public', express.static(process.cwd() + '/public'))
 app.set('view engine', 'ejs')
 
 routes(app)
