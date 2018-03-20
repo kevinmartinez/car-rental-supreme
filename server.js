1c74544a32f4a578a83afb6d93eb0076bf51da1a
kevinmartinez <kevin@pixelkojan.se>
2018-03-20 19:11:11 +0100

    WIP config routing, templating and sass middleware

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 'use strict'
 
 const express = require('express')
-const port = process.env.PORT || 3000
-const routes = require('./routes/index.js')
-// const sass = require('node-sass-middleware')
+const routes = require('./routes/index')
+const sass = require('node-sass-middleware')
 const path = require('path')
+const reload = require('reload')
 const app = express()
 
-// app.use(sass({
-//   /* Options */
-//   src: __dirname,
-//   dest: path.join(__dirname, 'public/css'),
-//   debug: true,
-//   outputStyle: 'compressed',
-//   prefix: '/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-// }))
-// Note: you must place sass-middleware *before* `express.static` or else it will
-// not work.
-app.use('/public', express.static(path.join(__dirname, '/public')))
-
-// app.use('/public', express.static(process.cwd() + '/public'))
+app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'ejs')
+
+// Set app head title
+app.locals.appTitle = 'Car Rental Supreme'
+app.locals.currentYear = new Date().getFullYear()
+
+// Sass middleware
+app.use(sass({
+  /* Options */
+  src: 'scss',
+  dest: path.join(__dirname, 'public/css'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}))
+// sass-middleware *before* `express.static`
+app.use(express.static(path.join(__dirname, '/public')))
 
 routes(app)
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+app.listen(app.get('port'))
+
+// Live reload
+reload(app, {verbose: true})
